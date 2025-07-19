b70e3568ff5edace3d98f04f7065c8adf38d5cac
Lajos Koszti <ajnasz@ajnasz.hu>
2025-07-19 17:26:28 +0200

    feat: add error handling

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"log"
 	"os"
 	"text/template"
 )
 
 func main() {
-	fileName := "example.tmpl"
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <template-file>\n", os.Args[0])
+	}
+
+	fileName := os.Args[1]
+
+	// Check if the file exists
+	if _, err := os.Stat(fileName); os.IsNotExist(err) {
+		panic("File does not exist: " + fileName)
+	}
 	templ := template.Must(template.New(fileName).ParseFiles(fileName))
-	if err := templ.ExecuteTemplate(os.Stdout, "example.tmpl", nil); err != nil {
-		panic(err)
+	if err := templ.ExecuteTemplate(os.Stdout, fileName, nil); err != nil {
+		log.Fatalf("Error executing template: %v\n", err)
 	}
 }
